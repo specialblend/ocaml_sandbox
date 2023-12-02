45a3ce07b5eaf3b5b43b4eaee7072dfb101ca2d4
CJ <cj@specialblend.dev>
2023-12-02 00:42:30 -0600

    feat: cubes

---
+++ bin/Cubes.ml
@@ -0,0 +1,38 @@
+type cube = Red of int | Green of int | Blue of int [@@deriving show]
+type game = int * cube list [@@deriving show]
+
+open Str
+
+let ( >> ) f g x = g (f x)
+let nth n l = List.nth l n
+let int = int_of_string
+let re = regexp
+
+let colon = re ":"
+and comma = re ", "
+and semicolon = re ";"
+and space = re " "
+
+let parse_cube = function
+  | [ qty; "red" ] -> Red (int qty)
+  | [ qty; "green" ] -> Green (int qty)
+  | [ qty; "blue" ] -> Blue (int qty)
+
+let parse_game =
+  split colon >> nth 1 >> split semicolon
+  >> List.concat_map (split comma >> List.map (split space >> parse_cube))
+
+let valid_cube (max_red, max_green, max_blue) = function
+  | Red qty -> qty <= max_red
+  | Green qty -> qty <= max_green
+  | Blue qty -> qty <= max_blue
+
+let valid_cubes thresholds = List.for_all (valid_cube thresholds)
+let valid_game thresholds (_, cubes) = valid_cubes thresholds cubes
+
+let () =
+  let thresholds = (12, 13, 14) in
+  Core.In_channel.read_lines "cubes.txt"
+  |> List.mapi (fun id line -> (id + 1, parse_game line))
+  |> List.filter (valid_game thresholds)
+  |> List.map fst |> List.fold_left ( + ) 0 |> print_int
