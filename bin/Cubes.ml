b2609f83483d4fdb298649655a39b5d8fbb7e18a
CJ <cj@specialblend.dev>
2023-12-02 00:59:52 -0600

    part 2

--- bin/Cubes.ml
+++ bin/Cubes.ml
@@ -7,6 +7,7 @@ let ( >> ) f g x = g (f x)
 let nth n l = List.nth l n
 let int = int_of_string
 let re = regexp
+let sum = List.fold_left ( + ) 0
 
 let colon = re ":"
 and comma = re ", "
@@ -30,9 +31,27 @@ let valid_cube (max_red, max_green, max_blue) = function
 let valid_cubes thresholds = List.for_all (valid_cube thresholds)
 let valid_game thresholds (_, cubes) = valid_cubes thresholds cubes
 
+let max_rgb =
+  let init = (0, 0, 0) in
+  List.fold_left
+    (fun (r, g, b) -> function
+      | Red qty -> (max r qty, g, b)
+      | Green qty -> (r, max g qty, b)
+      | Blue qty -> (r, g, max b qty))
+    init
+
+let power (_, cubes) =
+  let r, g, b = max_rgb cubes in
+  r * g * b
+
 let () =
   let thresholds = (12, 13, 14) in
   Core.In_channel.read_lines "cubes.txt"
   |> List.mapi (fun id line -> (id + 1, parse_game line))
   |> List.filter (valid_game thresholds)
-  |> List.map fst |> List.fold_left ( + ) 0 |> print_int |> print_newline
+  |> List.map fst |> sum |> print_int |> print_newline
+
+let () =
+  Core.In_channel.read_lines "cubes.txt"
+  |> List.mapi (fun id line -> (id + 1, parse_game line))
+  |> List.map power |> sum |> print_int |> print_newline
