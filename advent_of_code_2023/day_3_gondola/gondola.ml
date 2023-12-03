ce8a2be009f02b818b75e0ecd682aa6d96790253
CJ <cj@specialblend.dev>
2023-12-03 16:14:56 -0600

    clean up

--- advent_of_code_2023/day_3_gondola/gondola.ml
+++ advent_of_code_2023/day_3_gondola/gondola.ml
@@ -6,6 +6,7 @@ type expr =
 
 let pattern = "\\([0-9]+\\)\\|\\([^\\.]\\)"
 let scan_exprs = pattern |> Str.regexp |> Regex.find_all
+let len = string_of_int >> String.length
 
 let parse_expr y (expr, x) =
   match int_of_string_opt expr with
@@ -16,18 +17,18 @@ let parse_line y str = scan_exprs str |> List.map (parse_expr y)
 let parse_lines = List.mapi parse_line >> List.concat_map (fun l -> l)
 
 let is_adj (Int (e, x, y)) (Sym (_, x', y')) =
-  let len = String.length (string_of_int e) in
-  let is_adj_x = is_between (x - 1, x + len)
+  let l = len e in
+  let is_adj_x = is_between (x - 1, x + l)
   and is_adj_y = is_between (y - 1, y + 1) in
   is_adj_x x' && is_adj_y y'
 
 let is_part_num board = function
-  | Sym (_, _, _) -> false
+  | Sym _ -> false
   | num ->
       List.exists
         (function
-          | Sym (_, _, _) as sym -> is_adj num sym
-          | _ -> false)
+          | Int _ -> false
+          | sym -> is_adj num sym)
         board
 
 let parse_gear board = function
@@ -35,21 +36,21 @@ let parse_gear board = function
       Some
         (List.filter
            (function
-             | Int (_, _, _) as num -> is_adj num sym
+             | Int _ as num -> is_adj num sym
              | _ -> false)
            board)
   | _ -> None
 
 let value = function
   | Int (e, _, _) -> e
-  | Sym (_, _, _) -> 0
+  | _ -> 0
 
 let _ =
   let board = "gondola.txt" |> Core.In_channel.read_lines |> parse_lines in
   board
   |> List.filter (is_part_num board)
   |> List.map value
-  |> List.fold_left ( + ) 0
+  |> List.sum
   |> string_of_int
   |> print_endline
 
@@ -59,7 +60,7 @@ let _ =
   |> List.filter_map (parse_gear board)
   |> List.filter (List.length >> ( = ) 2)
   |> List.map (List.map value)
-  |> List.map (List.fold_left ( * ) 1)
-  |> List.fold_left ( + ) 0
+  |> List.map List.product
+  |> List.sum
   |> string_of_int
   |> print_endline
--- lib/Fun.ml
+++ lib/Fun.ml
@@ -1,2 +1,9 @@
 let ( >> ) f g x = g (f x)
 let is_between (min, max) x = min <= x && x <= max
+
+module List = struct
+  include List
+
+  let sum = fold_left ( + ) 0
+  let product = fold_left ( * ) 1
+end
