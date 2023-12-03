45cf486a2c143bdb3f95cc9fff790e3414de5b92
CJ <cj@specialblend.dev>
2023-12-03 15:18:13 -0600

    housekeeping

--- advent_of_code_2023/day_3_gondola/gondola.ml
+++ advent_of_code_2023/day_3_gondola/gondola.ml
@@ -4,21 +4,19 @@ type expr =
   | Int of int * int * int
   | Sym of string * int * int
 
-let scan_board = Regex.find_all (Str.regexp "\\([0-9]+\\)\\|\\([^\\.]\\)")
+let pattern = "\\([0-9]+\\)\\|\\([^\\.]\\)"
+let scan_exprs = pattern |> Str.regexp |> Regex.find_all
 
 let parse_expr y (expr, x) =
   match int_of_string_opt expr with
   | Some e -> Int (e, x, y)
   | None -> Sym (expr, x, y)
 
-let parse_line y s = scan_board s |> List.map (parse_expr y)
+let parse_line y str = scan_exprs str |> List.map (parse_expr y)
 let parse_lines = List.mapi parse_line >> List.concat_map (fun l -> l)
 
-(*  *)
-let is_between (min, max) x = x >= min && x <= max
-
-let is_part_num (board : expr list) =
-  let is_adjacent (e, x, y) =
+let is_part_num exprs =
+  let is_adj (e, x, y) =
     let len = String.length (string_of_int e) in
     let is_adj_x = is_between (x - 1, x + len)
     and is_adj_y = is_between (y - 1, y + 1) in
@@ -28,7 +26,7 @@ let is_part_num (board : expr list) =
   in
   function
   | Sym (_, _, _) -> false
-  | Int (e, x, y) -> List.exists (is_adjacent (e, x, y)) board
+  | Int (e, x, y) -> List.exists (is_adj (e, x, y)) exprs
 
 let value = function
   | Int (e, _, _) -> e
--- lib/Fun.ml
+++ lib/Fun.ml
@@ -1,1 +1,2 @@
 let ( >> ) f g x = g (f x)
+let is_between (min, max) x = min <= x && x <= max
