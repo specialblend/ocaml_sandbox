11f7c79004de68ff353cd0a5bb7d5a7bce1edf28
CJ <cj@specialblend.dev>
2023-12-03 14:37:13 -0600

    check adjacent

--- advent_of_code_2023/day_3_gondola/gondola.ml
+++ advent_of_code_2023/day_3_gondola/gondola.ml
@@ -9,8 +9,8 @@ let scan_board = Regex.find_all number_or_symbol
 
 let parse_expr y (expr, x) =
   match int_of_string_opt expr with
-  | Some e -> Int (e, x, y)
-  | None -> Sym (expr, x, y)
+  | Some e -> Int (e, x, y + 1)
+  | None -> Sym (expr, x, y + 1)
 
 let parse_line y s = scan_board s |> List.map (parse_expr y)
 let parse_lines = List.mapi parse_line >> List.concat_map (fun l -> l)
@@ -19,9 +19,20 @@ let _print_expr = function
   | Int (e, x, y) -> Printf.printf "%d:%d %d \n" y x e
   | Sym (e, x, y) -> Printf.printf "%d:%d %s \n" y x e
 
-let is_part_num (board : expr list) = function
-  | Int (_, _x, _y) -> List.exists (fun _ -> true) board
+let is_between (min, max) x = x >= min && x <= max
+
+let is_part_num (board : expr list) =
+  let is_adjacent (e, x, y) =
+    let len = String.length (string_of_int e) in
+    let is_adj_x = is_between (x - 1, x + len)
+    and is_adj_y = is_between (y - 1, y + 1) in
+    function
+    | Sym (_, x', y') -> is_adj_x x' && is_adj_y y'
+    | _ -> false
+  in
+  function
   | Sym (_, _, _) -> false
+  | Int (e, x, y) -> List.exists (is_adjacent (e, x, y)) board
 
 let value = function
   | Int (e, _, _) -> e
--- lib/Regex.ml
+++ lib/Regex.ml
@@ -3,13 +3,15 @@ open Str
 type t = string * int
 
 let find_all (r : regexp) (s : string) : t list =
+  let _l = String.length s in
   let rec find acc i =
     try
       let i = search_forward r s i in
       let s' = matched_string s in
-      let i = i + String.length s' in
       let m = (s', i) in
+      let i = i + String.length s' in
       find (m :: acc) i
-    with Not_found -> acc
+    with
+    | Not_found -> acc
   in
   find [] 0
