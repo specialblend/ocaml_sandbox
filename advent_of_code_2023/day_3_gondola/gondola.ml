32c284d7402c663ddb78dda0dee0a1a1c711ae24
CJ <cj@specialblend.dev>
2023-12-03 18:10:40 -0600

    simplify

--- advent_of_code_2023/day_3_gondola/gondola.ml
+++ advent_of_code_2023/day_3_gondola/gondola.ml
@@ -1,23 +1,20 @@
 open Fun
 
-type expr = string * int * int
-
-let parse_line (y : int) str : expr list =
-  str
-  |> Regex.find_all (Str.regexp "\\([0-9]+\\)\\|\\([^\\.]\\)")
-  |> List.map (fun (e, x) -> (e, x, y))
+let parse_line y str =
+  Regex.find_all (Str.regexp "\\([0-9]+\\)\\|\\([^\\.]\\)") str
+  |> List.map (fun (e, x) -> (e, (x, y)))
 
 let len = String.length
-let is_num (e, _, _) = int_of_string_opt e <> None
-let is_sym (e, _, _) = int_of_string_opt e = None
+let is_num (e, _) = int_of_string_opt e <> None
+let is_sym (e, _) = int_of_string_opt e = None
 
-let is_adj (e, x, y) (e', x', y') =
+let is_adj (e, (x, y)) (e', (x', y')) =
   is_between (x - len e', x + len e) x' && is_between (y - 1, y + 1) y'
 
 let is_part_num board num = List.exists (is_adj num) (List.filter is_sym board)
 
 let parse_gear board = function
-  | ("*", _, _) as gear -> List.filter (is_adj gear) (List.filter is_num board)
+  | ("*", _) as gear -> List.filter (is_adj gear) (List.filter is_num board)
   | _ -> []
 
 let read_board =
@@ -27,7 +24,7 @@ let _ =
   let board = read_board "gondola.txt" in
   List.filter is_num board
   |> List.filter (is_part_num board)
-  |> List.filter_map (fun (e, _, _) -> int_of_string_opt e)
+  |> List.filter_map (fst >> int_of_string_opt)
   |> List.sum
   |> string_of_int
   |> print_endline
@@ -37,7 +34,7 @@ let _ =
   board
   |> List.map (parse_gear board)
   |> List.filter (List.length >> ( = ) 2)
-  |> List.map (List.filter_map (fun (e, _, _) -> int_of_string_opt e))
+  |> List.map (List.filter_map (fst >> int_of_string_opt))
   |> List.map List.product
   |> List.sum
   |> string_of_int
