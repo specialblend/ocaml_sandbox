6e1cac9d2e4081fba6a98b0c37a3ccd00c6e42e5
CJ <cj@specialblend.dev>
2023-12-04 16:10:14 -0600

    Update cards.ml

--- advent_of_code_2023/day_4_cards/cards.ml
+++ advent_of_code_2023/day_4_cards/cards.ml
@@ -1,19 +1,21 @@
 open Fun
 
-let parse_line =
-  Str.split (Str.regexp ":")
-  >> List.nth 1
-  >> Str.split (Str.regexp "|")
-  >> List.map (Str.split (Str.regexp "[ ]+"))
-  >> fun [ left; right ] -> (left, right)
+let parse_line s =
+  Str.split (Str.regexp ":") s
+  |> List.nth 1
+  |> Str.split (Str.regexp "|")
+  |> List.map (Str.split (Str.regexp "[ ]+"))
+  |> fun [ left; right ] -> (left, right)
 
 let points n = int_of_float (2.0 ** float_of_int (n - 1))
 
-let scan_matches (left, right) =
-  left |> List.filter (fun card -> right |> List.exists (( = ) card))
+let count_cards (left, right) =
+  left
+  |> List.filter (fun card -> right |> List.exists (( = ) card))
+  |> List.length
+  |> points
 
 let _ =
-  let count_cards = scan_matches >> List.length >> points in
   Core.In_channel.read_lines "cards.txt"
   |> List.map parse_line
   |> List.map count_cards
