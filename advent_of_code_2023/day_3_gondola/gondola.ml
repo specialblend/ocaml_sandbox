95d7127c3a05a16468128b4a00d47959b866e066
CJ <cj@specialblend.dev>
2023-12-04 17:17:36 -0600

    Update gondola.ml

--- advent_of_code_2023/day_3_gondola/gondola.ml
+++ advent_of_code_2023/day_3_gondola/gondola.ml
@@ -1,10 +1,11 @@
 open Fun
 
-let parse_line y str =
-  let exp = "\\([0-9]+\\)\\|\\([^\\.]\\)" in
-  Regex.find_all (Str.regexp exp) str |> List.map (fun (e, x) -> (e, (x, y)))
+let parse_line y string =
+  let pattern = Str.regexp "\\([0-9]+\\)\\|\\([^\\.]\\)" in
+  let fmt_expr (e, x) = (e, (x, y)) in
+  string |> Regex.find_all pattern |> List.map fmt_expr
 
-let evaluate (e, _) = int_of_string_opt e
+let value (e, _) = int_of_string_opt e
 let is_sym (e, _) = is_numeric e
 let is_num (e, l) = not (is_sym (e, l))
 let is_gear_symbol (e, _) = e = "*"
@@ -18,24 +19,43 @@ let is_adjacent expr1 expr2 =
 let read_board filename =
   Core.In_channel.read_lines filename |> List.mapi parse_line |> List.flatten
 
-let _ =
-  let board = read_board "gondola.txt" in
-  let symbols = List.filter is_sym board in
-  List.filter is_num board
+let () =
+  let expressions =
+    "gondola.txt"
+    |> Core.In_channel.read_lines
+    |> List.mapi parse_line
+    |> List.flatten
+  in
+  let symbols = expressions |> List.filter is_sym
+  and numbers = expressions |> List.filter is_num in
+  numbers
   |> List.filter (fun number -> List.exists (is_adjacent number) symbols)
-  |> List.filter_map evaluate
+  |> List.filter_map value
   |> List.sum
   |> string_of_int
   |> print_endline
 
-let _ =
-  let board = read_board "gondola.txt" in
-  let numbers = List.filter is_num board in
-  List.filter is_gear_symbol board
-  |> List.map (fun symbol -> List.filter (is_adjacent symbol) numbers)
-  |> List.filter (fun parts -> List.length parts = 2)
-  |> List.map (List.filter_map evaluate)
-  |> List.map List.product
-  |> List.sum
+let () =
+  let expressions =
+    "gondola.txt"
+    |> Core.In_channel.read_lines
+    |> List.mapi parse_line
+    |> List.flatten
+  in
+  let numbers = List.filter is_num expressions in
+  let find_gears expressions =
+    expressions
+    |> List.filter is_gear_symbol
+    |> List.map (fun symbol -> List.filter (is_adjacent symbol) numbers)
+    |> List.filter (fun parts -> List.length parts = 2)
+  and calculate_gear_ratios gears =
+    gears
+    |> List.map (List.filter_map value)
+    |> List.map List.product
+    |> List.sum
+  in
+  expressions
+  |> find_gears
+  |> calculate_gear_ratios
   |> string_of_int
   |> print_endline
