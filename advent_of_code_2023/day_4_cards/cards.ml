cbd38314238be08f9c1277e1f408a996d49422fb
CJ <cj@specialblend.dev>
2023-12-04 21:27:18 -0600

    part 2

--- advent_of_code_2023/day_4_cards/cards.ml
+++ advent_of_code_2023/day_4_cards/cards.ml
@@ -27,3 +27,29 @@ let _ =
   |> List.sum
   |> string_of_int
   |> print_endline
+
+let _print_counts counts =
+  counts
+  |> Array.map string_of_int
+  |> Array.to_list
+  |> String.concat ","
+  |> print_endline
+
+let _ =
+  let cards =
+    "cards.txt"
+    |> Core.In_channel.read_lines
+    |> List.map parse_line
+    |> List.map count_cards
+  in
+  let counts = Array.make (List.length cards) 1 in
+  let incr_by n i = counts.(i) <- counts.(i) + n in
+
+  cards
+  |> List.mapi (fun index card -> (index, card))
+  |> List.iter (fun (index, count) ->
+         for i = index + 1 to index + count do
+           let n = counts.(index) in
+           incr_by n i
+         done);
+  counts |> Array.to_list |> List.sum |> string_of_int |> print_endline
