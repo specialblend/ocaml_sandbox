b95d40262e4547bd67e8f88a5c95cc00d1056649
CJ <cj@specialblend.dev>
2023-12-04 22:24:45 -0600

    Update cards.ml

--- advent_of_code_2023/day_4_cards/cards.ml
+++ advent_of_code_2023/day_4_cards/cards.ml
@@ -13,40 +13,40 @@ let parse_line str =
 
 let points n = int_of_float (2.0 ** float_of_int (n - 1))
 
-let count_cards (left, right) =
+let count_wins (left, right) =
   left |> List.filter (fun card -> right |> List.mem card) |> List.length
 
 let _ =
   "cards.txt"
   |> Core.In_channel.read_lines
   |> List.map parse_line
-  |> List.map count_cards
+  |> List.map count_wins
   |> List.map points
   |> List.sum
   |> string_of_int
   |> print_endline
 
 let _ =
-  let cards =
+  let winning_cards =
     "cards.txt"
     |> Core.In_channel.read_lines
     |> List.map parse_line
-    |> List.map count_cards
+    |> List.map count_wins
   in
 
-  let init_counter = Array.make (List.length cards) 1 in
+  let init_counter = Array.make (List.length winning_cards) 1 in
 
-  let count_copies counter (index, count) =
+  let count_copies counter (index, win_count) =
     let n = counter.(index) in
     let _ =
-      for k = index + 1 to index + count do
+      for k = index + 1 to index + win_count do
         counter.(k) <- counter.(k) + n
       done
     in
     counter
   in
 
-  cards
+  winning_cards
   |> List.mapi pair
   |> List.fold_left count_copies init_counter
   |> Array.to_list
