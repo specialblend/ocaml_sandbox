b55f80d41f01d40a471591bc4852319ed7dae1c2
CJ <cj@specialblend.dev>
2023-12-09 12:54:03 -0600

    test

--- advent_of_code_2023/day_5_seeds/seeds_fun_test.ml
+++ advent_of_code_2023/day_5_seeds/seeds_fun_test.ml
@@ -339,21 +339,21 @@ module Range_test = struct
     | Some (Subset (3, 5)) -> true
     | _ -> false
 
-  (* let%test "range intersect full subset" =
-     let range1 = (1, 10) in
-     let range2 = (1, 10) in
+  let%test "range intersect full subset" =
+    let range1 = (1, 10) in
+    let range2 = (1, 10) in
 
-     match Seeds_fun.Range.intersect range1 range2 with
-     | Some (Subset (1, 10)) -> true
-     | _ -> false *)
+    match Seeds_fun.Range.intersect range1 range2 with
+    | Some (Subset (1, 10)) -> true
+    | _ -> false
 
-  (* let%test "range intersect superset" =
-     let range1 = (3, 5) in
-     let range2 = (1, 10) in
+  let%test "range intersect superset" =
+    let range1 = (3, 5) in
+    let range2 = (1, 10) in
 
-     match Seeds_fun.Range.intersect range1 range2 with
-     | Some (Superset (1, 10)) -> true
-     | _ -> false *)
+    match Seeds_fun.Range.intersect range1 range2 with
+    | Some (Superset (1, 10)) -> true
+    | _ -> false
 
   let%test "range intersect overlap right" =
     let range1 = (1, 10) in
