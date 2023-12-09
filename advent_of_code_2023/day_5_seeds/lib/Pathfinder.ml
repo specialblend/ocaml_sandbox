b8a3d3ee86d8dbe67d38de9ecf1a1d35f639fa82
CJ <cj@specialblend.dev>
2023-12-09 15:13:38 -0600

    syntax

--- advent_of_code_2023/day_5_seeds/lib/Pathfinder.ml
+++ advent_of_code_2023/day_5_seeds/lib/Pathfinder.ml
@@ -37,48 +37,45 @@ let use_subset (path, range) row _ =
   let result' = (path', range') in
   Some result'
 
-let fold_table result table =
+let fold_table init_result init_table =
   let rec fold (result, table) =
     match (result, table) with
-    | Some result, rows :: table ->
-        let _, range = result in
-        let fold_row row =
-          let a, b = range_of row in
-          let result' =
-            match Range.intersect (a, b) range with
-            | Some (Subset (x, y)) ->
-                let result = use_subset result row (x, y) in
-                result
-            | Some (Overlap (x, y)) ->
-                let result = use_overlap result row (x, y) in
-                result
-            | _ -> None
-          in
-          fold (result', table)
-        in
-        rows
-        |>| List.find_opt (row_intersects range)
-        |>| fun row -> Option.bind row fold_row
+    | Some prev_result, rows :: table_rest -> begin
+        let _, range = prev_result in
+        match List.find_opt (row_intersects range) rows with
+        | Some row -> begin
+            let a, b = range_of row in
+            let next_result =
+              match Range.intersect (a, b) range with
+              | Some (Subset (x, y)) -> use_subset prev_result row (x, y)
+              | Some (Overlap (x, y)) -> use_overlap prev_result row (x, y)
+              | _ -> None
+            in
+            fold (next_result, table_rest)
+          end
+        | None -> None
+      end
     | _ -> result
   in
-  fold (result, table)
+  fold (init_result, init_table)
 
-let compile_header header =
+let compile_header : int * int * int -> t * (int * int) =
+ fun header ->
   let dst, src, margin = header in
   let window = (src, src + margin - 1) in
   let offset = dst - src in
   let range = Range.add offset window in
   let path = { window; offset } in
-  let result = (path, range) in
-  result
+  let init_result = (path, range) in
+  init_result
 
-let compile_table : table -> t list = function
-  | init :: table ->
-      List.filter_map
-        (fun header ->
-          let result = compile_header header in
-          match table |> fold_table (Some result) with
-          | Some (path, _) -> Some path
-          | None -> None)
-        init
+let compile_table : table -> t list =
+  let use_header table header =
+    let init_result = compile_header header in
+    match fold_table (Some init_result) table with
+    | Some (path, _) -> Some path
+    | None -> None
+  in
+  function
+  | init :: table -> List.filter_map (use_header table) init
   | _ -> failwith "illegal"
