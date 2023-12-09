3bf822e94583b5369781e917443a7f918d15eae6
CJ <cj@specialblend.dev>
2023-12-09 14:26:10 -0600

    introduce rec inner func

--- advent_of_code_2023/day_5_seeds/lib/Pathfinder.ml
+++ advent_of_code_2023/day_5_seeds/lib/Pathfinder.ml
@@ -37,28 +37,31 @@ let use_subset (path, range) row _ =
   let result' = (path', range') in
   Some result'
 
-let rec fold_table_into result table =
-  match (result, table) with
-  | None, _ -> None
-  | Some result, rows :: table ->
-      let _, range = result in
-      let fold_row row =
-        let result' =
-          match Range.intersect (range_of row) range with
-          | Some (Subset (x, y)) ->
-              let result = use_subset result row (x, y) in
-              result
-          | Some (Overlap (x, y)) ->
-              let result = use_overlap result row (x, y) in
-              result
-          | _ -> None
+let fold_table result table =
+  let rec fold = function
+    | None, _ -> None
+    | Some result, rows :: table ->
+        let _, range = result in
+        let fold_row row =
+          let a, b = range_of row in
+          let result' =
+            match Range.intersect (a, b) range with
+            | Some (Subset (x, y)) ->
+                let result = use_subset result row (x, y) in
+                result
+            | Some (Overlap (x, y)) ->
+                let result = use_overlap result row (x, y) in
+                result
+            | _ -> None
+          in
+          fold (result', table)
         in
-        fold_table_into result' table
-      in
-      rows
-      |>| List.find_opt (row_intersects range)
-      |>| fun row -> Option.bind row fold_row
-  | _ -> result
+        rows
+        |>| List.find_opt (row_intersects range)
+        |>| fun row -> Option.bind row fold_row
+    | _ -> result
+  in
+  fold (result, table)
 
 let compile_header header =
   let dst, src, margin = header in
@@ -69,13 +72,13 @@ let compile_header header =
   let result = (path, range) in
   result
 
-let compile_table : table -> t list = function
-  | init :: table ->
-      List.filter_map
-        (fun header ->
-          let result = compile_header header in
-          match table |> fold_table_into (Some result) with
-          | Some (path, _) -> Some path
-          | None -> None)
-        init
+let compile_table : table -> t list =
+  let use_header table header =
+    let result = compile_header header in
+    match table |> fold_table (Some result) with
+    | Some (path, _) -> Some path
+    | None -> None
+  in
+  function
+  | headers :: table -> List.filter_map (use_header table) headers
   | _ -> failwith "illegal"
