4236cabe3ae9fbbf6c4de59af36c16e96f35fc76
CJ <cj@specialblend.dev>
2023-12-11 22:20:12 -0600

    test

--- advent_of_code_2023/day_5_seeds/lib/Range.ml
+++ advent_of_code_2023/day_5_seeds/lib/Range.ml
@@ -18,6 +18,8 @@ let intersect (a, b) = function
   | x, y when x <= a && a <= y -> Some (OverlapLeft (a, y))
   | _ -> None
 
+let ( ^ ) = intersect
+
 let union ((a, b) as r1) ((x, y) as r2) =
   match intersect r1 r2 with
   | Some (Subset _) -> Some (a, b)
@@ -25,6 +27,8 @@ let union ((a, b) as r1) ((x, y) as r2) =
   | Some (OverlapRight _) -> Some (a, y)
   | Some (OverlapLeft _) -> Some (x, b)
   | None -> None
+
+let ( ++ ) = union
 
 let union_all rgs =
   let rec union_all = function
@@ -37,11 +41,13 @@ let union_all rgs =
   in
   union_all rgs
 
-type diff =
-  | Empty
-  | Unit of t
-  | Pair of t * t
-[@@deriving show]
+(*
+   type diff =
+     | Empty
+     | Unit of t
+     | Pair of t * t
+   [@@deriving show]
+*)
 
 let diff (a, b) (x, y) =
   match intersect (a, b) (x, y) with
@@ -54,16 +60,18 @@ let diff (a, b) (x, y) =
   | Some (OverlapLeft (_, y)) -> [ (y + 1, b) ]
   | None -> [ (a, b) ]
 
-let diff2 (a, b) (x, y) =
-  match intersect (a, b) (x, y) with
-  | _ when a = x && b = y -> Empty
-  | Some (Superset _) -> Empty
-  | Some (Subset (x, y)) when x = a -> Unit (y + 1, b)
-  | Some (Subset (x, y)) when y = b -> Unit (a, x - 1)
-  | Some (Subset (x, y)) -> Pair ((a, x - 1), (y + 1, b))
-  | Some (OverlapRight (x, _)) -> Unit (a, x - 1)
-  | Some (OverlapLeft (_, y)) -> Unit (y + 1, b)
-  | None -> Unit (a, b)
+(*
+   let diff2 (a, b) (x, y) =
+     match intersect (a, b) (x, y) with
+     | _ when a = x && b = y -> Empty
+     | Some (Superset _) -> Empty
+     | Some (Subset (x, y)) when x = a -> Unit (y + 1, b)
+     | Some (Subset (x, y)) when y = b -> Unit (a, x - 1)
+     | Some (Subset (x, y)) -> Pair ((a, x - 1), (y + 1, b))
+     | Some (OverlapRight (x, _)) -> Unit (a, x - 1)
+     | Some (OverlapLeft (_, y)) -> Unit (y + 1, b)
+     | None -> Unit (a, b)      
+*)
 
 let diff_all r ranges =
   let rec diff_all r1 rest =
@@ -73,9 +81,9 @@ let diff_all r ranges =
     |>| function
     | [] -> r1 :: []
     | r2 :: rest ->
-    match diff2 r1 r2 with
-    | Empty -> []
-    | Unit r3 -> diff_all r3 rest
-    | Pair (r3, r4) -> r3 :: diff_all r4 rest
+    match diff r1 r2 with
+    | [] -> []
+    | r3 :: [] -> diff_all r3 rest
+    | r3 :: r4 :: _ -> r3 :: diff_all r4 rest
   in
   diff_all r ranges
--- advent_of_code_2023/day_5_seeds/tests/range_test.ml
+++ advent_of_code_2023/day_5_seeds/tests/range_test.ml
@@ -53,15 +53,14 @@ module Diff_test = struct
   let%test _ = Range.diff (1, 9) (3, 10) = [ (1, 2) ]
   let%test _ = Range.diff (2, 10) (1, 9) = [ (10, 10) ]
 
-  (*  *)
-  let%test _ = Range.diff2 (3, 5) (1, 10) = Empty
-  let%test _ = Range.diff2 (1, 10) (1, 10) = Empty
-  let%test _ = Range.diff2 (1, 10) (3, 5) = Pair ((1, 2), (6, 10))
-  let%test _ = Range.diff2 (1, 5) (5, 10) = Unit (1, 4)
-  let%test _ = Range.diff2 (5, 10) (1, 5) = Unit (6, 10)
-  let%test _ = Range.diff2 (5, 10) (1, 9) = Unit (10, 10)
-  let%test _ = Range.diff2 (1, 9) (3, 10) = Unit (1, 2)
-  let%test _ = Range.diff2 (2, 10) (1, 9) = Unit (10, 10)
+  (* let%test _ = Range.diff2 (3, 5) (1, 10) = Empty
+     let%test _ = Range.diff2 (1, 10) (1, 10) = Empty
+     let%test _ = Range.diff2 (1, 10) (3, 5) = Pair ((1, 2), (6, 10))
+     let%test _ = Range.diff2 (1, 5) (5, 10) = Unit (1, 4)
+     let%test _ = Range.diff2 (5, 10) (1, 5) = Unit (6, 10)
+     let%test _ = Range.diff2 (5, 10) (1, 9) = Unit (10, 10)
+     let%test _ = Range.diff2 (1, 9) (3, 10) = Unit (1, 2)
+     let%test _ = Range.diff2 (2, 10) (1, 9) = Unit (10, 10) *)
 
   (* let%expect_test _ =
      Range.diff2 (1, 100) (1, 10) |> Range.show_diff |> print_endline;
@@ -83,11 +82,21 @@ module Diff_test = struct
     and expected = [ (1, 9); (21, 29); (41, 49); (61, 69); (81, 89) ] in
     Range.diff_all (1, 100) ranges = expected
 
-  (*  *)
-  (* let%expect_test _ =
-     Range.diff_all (1, 100)
-       [ (10, 20); (30, 40); (50, 60); (70, 80); (90, 100) ]
-     |> List.map Range.show
-     |> List.iter print_endline;
-     [%expect {||}] *)
+  let%test _ =
+    let ranges =
+      [ (10, 20); (30, 40); (50, 60); (35, 37); (70, 80); (90, 100); (11, 15) ]
+    and expected = [ (1, 9); (21, 29); (41, 49); (61, 69); (81, 89) ] in
+    Range.diff_all (1, 100) ranges = expected
+
+  let%expect_test _ =
+    let ranges = [ (10, 20); (30, 40); (50, 60); (70, 80); (90, 100) ] in
+    Range.diff_all (1, 100) ranges
+    |> List.map Range.show
+    |> List.iter print_endline;
+    [%expect {|
+      (1, 9)
+      (21, 29)
+      (41, 49)
+      (61, 69)
+      (81, 89) |}]
 end
