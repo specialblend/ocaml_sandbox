3ad8888bdaa890fa0ac3ad75830fb49a1ea162bd
CJ <cj@specialblend.dev>
2023-12-11 22:54:27 -0600

    simplify

--- advent_of_code_2023/day_5_seeds/lib/Range.ml
+++ advent_of_code_2023/day_5_seeds/lib/Range.ml
@@ -12,23 +12,23 @@ type intersect =
 [@@deriving show { with_path = false }]
 
 let intersect (a, b) = function
+  | x, y when a <= x && y <= b -> Some (x, y)
+  | x, y when x <= a && b <= y -> Some (a, b)
+  | x, y when x <= b && b <= y -> Some (x, b)
+  | x, y when x <= a && a <= y -> Some (a, y)
+  | _ -> None
+
+let intersect2 (a, b) = function
   | x, y when a <= x && y <= b -> Some (Subset (x, y))
   | x, y when x <= a && b <= y -> Some (Superset (x, y))
   | x, y when x <= b && b <= y -> Some (OverlapRight (x, b))
   | x, y when x <= a && a <= y -> Some (OverlapLeft (a, y))
   | _ -> None
 
-let ( ^ ) = intersect
-
 let union ((a, b) as r1) ((x, y) as r2) =
   match intersect r1 r2 with
-  | Some (Subset _) -> Some (a, b)
-  | Some (Superset _) -> Some (x, y)
-  | Some (OverlapRight _) -> Some (a, y)
-  | Some (OverlapLeft _) -> Some (x, b)
+  | Some _ -> Some (min a x, max b y)
   | None -> None
-
-let ( ++ ) = union
 
 let union_all rgs =
   let rec union_all = function
@@ -41,44 +41,21 @@ let union_all rgs =
   in
   union_all rgs
 
-(*
-   type diff =
-     | Empty
-     | Unit of t
-     | Pair of t * t
-   [@@deriving show]
-*)
-
-let diff (a, b) (x, y) =
-  match intersect (a, b) (x, y) with
-  | _ when a = x && b = y -> []
-  | Some (Superset _) -> []
-  | Some (Subset (x, y)) when x = a -> [ (y + 1, b) ]
-  | Some (Subset (x, y)) when y = b -> [ (a, x - 1) ]
-  | Some (Subset (x, y)) -> [ (a, x - 1); (y + 1, b) ]
-  | Some (OverlapRight (x, _)) -> [ (a, x - 1) ]
-  | Some (OverlapLeft (_, y)) -> [ (y + 1, b) ]
-  | None -> [ (a, b) ]
-
-(*
-   let diff2 (a, b) (x, y) =
-     match intersect (a, b) (x, y) with
-     | _ when a = x && b = y -> Empty
-     | Some (Superset _) -> Empty
-     | Some (Subset (x, y)) when x = a -> Unit (y + 1, b)
-     | Some (Subset (x, y)) when y = b -> Unit (a, x - 1)
-     | Some (Subset (x, y)) -> Pair ((a, x - 1), (y + 1, b))
-     | Some (OverlapRight (x, _)) -> Unit (a, x - 1)
-     | Some (OverlapLeft (_, y)) -> Unit (y + 1, b)
-     | None -> Unit (a, b)      
-*)
+let diff r1 r2 =
+  let a, b = r1
+  and x, y = r2 in
+  match intersect r1 r2 with
+  | _ when r1 = r2 -> []
+  | Some superset when superset = r1 -> []
+  | Some (x, y) when x = a -> [ (y + 1, b) ]
+  | Some (x, y) when y = b -> [ (a, x - 1) ]
+  | Some subset when subset = r2 -> [ (a, x - 1); (y + 1, b) ]
+  | Some (x, _) when x <= b -> [ (a, x - 1) ]
+  | Some (_, y) -> [ (y + 1, b) ]
+  | None -> [ r1 ]
 
 let diff_all r ranges =
-  let rec diff_all r1 rest =
-    rest
-    |>| union_all
-    |>| List.sort compare
-    |>| function
+  let rec diff_all r1 = function
     | [] -> r1 :: []
     | r2 :: rest ->
     match diff r1 r2 with
@@ -86,4 +63,4 @@ let diff_all r ranges =
     | r3 :: [] -> diff_all r3 rest
     | r3 :: r4 :: _ -> r3 :: diff_all r4 rest
   in
-  diff_all r ranges
+  diff_all r (ranges |> union_all |> List.sort compare)
--- advent_of_code_2023/day_5_seeds/tests/range_test.ml
+++ advent_of_code_2023/day_5_seeds/tests/range_test.ml
@@ -1,12 +1,12 @@
 open Seeds
 
 module Intersect_test = struct
-  let%test _ = Range.intersect (1, 10) (3, 5) = Some (Subset (3, 5))
-  let%test _ = Range.intersect (1, 10) (1, 10) = Some (Subset (1, 10))
-  let%test _ = Range.intersect (3, 5) (1, 10) = Some (Superset (1, 10))
-  let%test _ = Range.intersect (1, 10) (4, 12) = Some (OverlapRight (4, 10))
-  let%test _ = Range.intersect (5, 13) (2, 7) = Some (OverlapLeft (5, 7))
-  let%test _ = Range.intersect (1, 13) (21, 47) = None
+  let%test _ = Range.intersect2 (1, 10) (3, 5) = Some (Subset (3, 5))
+  let%test _ = Range.intersect2 (1, 10) (1, 10) = Some (Subset (1, 10))
+  let%test _ = Range.intersect2 (3, 5) (1, 10) = Some (Superset (1, 10))
+  let%test _ = Range.intersect2 (1, 10) (4, 12) = Some (OverlapRight (4, 10))
+  let%test _ = Range.intersect2 (5, 13) (2, 7) = Some (OverlapLeft (5, 7))
+  let%test _ = Range.intersect2 (1, 13) (21, 47) = None
 end
 
 module Union_test = struct
@@ -52,6 +52,7 @@ module Diff_test = struct
   let%test _ = Range.diff (5, 10) (1, 9) = [ (10, 10) ]
   let%test _ = Range.diff (1, 9) (3, 10) = [ (1, 2) ]
   let%test _ = Range.diff (2, 10) (1, 9) = [ (10, 10) ]
+  let%test _ = Range.diff (50, 100) (25, 75) = [ (76, 100) ]
 
   (* let%test _ = Range.diff2 (3, 5) (1, 10) = Empty
      let%test _ = Range.diff2 (1, 10) (1, 10) = Empty
@@ -93,7 +94,8 @@ module Diff_test = struct
     Range.diff_all (1, 100) ranges
     |> List.map Range.show
     |> List.iter print_endline;
-    [%expect {|
+    [%expect
+      {|
       (1, 9)
       (21, 29)
       (41, 49)
