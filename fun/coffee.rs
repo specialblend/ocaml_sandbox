9997092a8d7d73f6714c5dbfb41e7cbf2cb53cc5
CJ <cj@specialblend.dev>
2023-12-12 18:08:15 -0600

    Update coffee.rs

--- fun/coffee.rs
+++ fun/coffee.rs
@@ -1,8 +1,11 @@
+
 fn handle(cup: Cup) {
   let action = match cup {
       Some(coffee) => drink(coffee),
       None => cup.fill("☕️")
   };
+
   handle(action);
+  
   sleep(Duration::SECOND);
 }
